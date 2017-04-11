61fbb4d70a6fce22ee9483aa4adf6389c2284c3f
TimE1211 <timmy121192@gmail.com>
2017-04-11 10:07:08 -0400

    finished routes

--- Leaderboard/Sources/main.swift
+++ Leaderboard/Sources/main.swift
@@ -3,3 +3,24 @@ import PerfectHTTP
 import PerfectHTTPServer
 
 
+let server = HTTPServer()
+
+var routes = Routes()     //object to store each route/endpoint
+
+routes.add(method: .get, uri: "/", handler: {
+  request, response in
+  response.setHeader(.contentType, value: "text/html")
+  response.appendBody(string: "<html><title>Hello, World!</title><body>Hello, World!</body></html>")
+  response.completed()
+})
+
+server.addRoutes(routes)
+
+server.serverPort = 8181
+
+do {
+  try server.start()
+} catch PerfectError.networkError(let error, let msg)
+{
+  print("Network error thrown: \(error) \(msg)")
+}
