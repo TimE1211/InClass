fe34a9ea97d57389868af44e6b81761a961117ee
TimE1211 <timmy121192@gmail.com>
2017-04-11 11:56:11 -0400

    finished save score

--- Leaderboard/Sources/main.swift
+++ Leaderboard/Sources/main.swift
@@ -5,6 +5,8 @@ import PerfectRequestLogger
 import SQLiteStORM
 
 let connect = SQLiteConnect("./scoresdb")   //      ./=current folder       db = database 
+let scores = Score(connect)
+scores.setup()
 
 let server = HTTPServer()
 
@@ -20,6 +22,8 @@ routes.add(method: .get, uri: "/", handler: {
   response.appendBody(string: "<html><title>Hello, World!</title><body>Hello, World!</body></html>")
   response.completed()
 })
+
+routes.add(method: .post, uri: "api/v1/save", handler: processSaveScore)
 
 server.addRoutes(routes)
 
